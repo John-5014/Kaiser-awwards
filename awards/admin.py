7ef6ad6a3ee18a7c1e334d05a0c6d4b3d5653c94
John-5014 <kaiserjohn52@gmail.com>
2019-11-23 15:37:16 +0300

    register models

--- awards/admin.py
+++ awards/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+class ProjectAdmin(admin.ModelAdmin):
+  filter_horizontal = ('technologies', 'categories')
+
+
+admin.site.register(Project)
+admin.site.register(categories)
+admin.site.register(technologies)
+admin.site.register(Profile)
+
