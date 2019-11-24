bcc8ecf104d4d461e32bbc71afbcf49444f82bea
John-5014 <kaiserjohn52@gmail.com>
2019-11-24 20:38:49 +0300

    adds users url

--- clone/urls.py
+++ clone/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from django.contrib.auth import views 
+from users import views as user_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('awards.urls')),
+    url(r'^register/', user_views.register, name='register'),
+    # url(r'^accounts/', include('registration.backends.simple.urls')),
+    # url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
