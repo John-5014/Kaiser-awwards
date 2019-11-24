ee698456ab8f8832ef8a3e1479c566bad8d3505b
John-5014 <kaiserjohn52@gmail.com>
2019-11-24 20:42:26 +0300

    few changes to base files and login

---
+++ awards/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import *
+
+
+# class UploadForm(forms.ModelForm):
+
+#   class Meta:
+
+#     model = Project
+#     exclude = ('design', 'usability', 'creativity''content', 'overall', 'posted', 'user' )
+
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ('pic','bio', 'nickname','email')
+
+# class RatingForm(forms.ModelForm):
+#   class Meta:
+#     model=Rating
+#     exclude=['overall_score','profile','project']
