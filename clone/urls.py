fdd3b180a337e9d60566ea3c4186a44e12aa327a
John-5014 <kaiserjohn52@gmail.com>
2019-11-25 08:14:15 +0300

    adds new profile route to page for rendering

--- clone/urls.py
+++ clone/urls.py
@@ -17,11 +17,15 @@ from django.conf.urls import url,include
 from django.contrib import admin
 from django.contrib.auth import views 
 from users import views as user_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'',include('awards.urls')),
     url(r'^register/', user_views.register, name='register'),
+    url(r'^profile/', user_views.profile, name='profile'),
+    url(r'^login/', auth_views.LoginView.as_view(template_name= 'users/login.html'), name='login'),
+    url(r'^logout/', auth_views.LogoutView.as_view(template_name= 'users/logout.html'), name='logout'),
     # url(r'^accounts/', include('registration.backends.simple.urls')),
     # url(r'^logout/$', views.logout, {"next_page": '/'}),
 ]
